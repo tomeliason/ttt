89bc991e81ca60dc63a302fe56201933fefc49df
tom.eliason <tom.eliason@oracle.com>
2020-08-26 20:18:49 -0600

    testing clean

--- cleancompartment/cleancompartment.sh
+++ cleancompartment/cleancompartment.sh
@@ -1,12 +1,14 @@
-#!/usr/bin/env bash -ex
+#!/bin/bash -ex
+
+echo 'cleancompartment' 
 
 export TF_VAR_tenancy_ocid=$OCI_TENANCY
 
-cd gbuocid
+cd ttt/cleancompartment/gbuocid
 
 terraform init
 terraform apply -auto-approve
-export TF_VAR_compartment_id = `terraform output tenancycompartments`
+export TF_VAR_compartment_id=`terraform output tenancycompartments`
 
 cd .. 
 
@@ -16,12 +18,15 @@ rm -rf tf-export
 
 mkdir tf-export 
 
-#./terraform-provider-oci -command=export \
-#                         -#compartment_id=ocid1.compartment.oc1..aaaaaaaaas357j3xdy5grl4dmotmtumrcopqxifuayurkowavjvzrmihfala \
-#                         -output_path=tf-export \
-#                         -generate_state
+./terraform-provider-oci -command=export \
+                         -compartment_id=ocid1.compartment.oc1..aaaaaaaaas357j3xdy5grl4dmotmtumrcopqxifuayurkowavjvzrmihfala \
+                         -output_path=tf-export \
+                         -generate_state
 
-#cd tf-export
+cd tf-export
 
-#terraform init
-#terraform destroy -auto-approve
+terraform init
+terraform destroy -auto-approve
+
+echo 'cleancompartment complete'
+
