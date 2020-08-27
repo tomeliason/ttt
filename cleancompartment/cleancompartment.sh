8caebc8a6c28f069442c6c0718512b25764933ab
tom.eliason <tom.eliason@oracle.com>
2020-08-26 20:01:45 -0600

    get gbu compartment ocid, pass to script

--- cleancompartment/cleancompartment.sh
+++ cleancompartment/cleancompartment.sh
@@ -1,15 +1,24 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -ex
+
+export TF_VAR_tenancy_ocid=$OCI_TENANCY
+
+cd gbuocid
+
+terraform init
+terraform apply -auto-approve
+export TF_VAR_compartment_id = `terraform output tenancycompartments`
+
 
 rm -rf tf-export 
 
 mkdir tf-export 
 
-./terraform-provider-oci -command=export \
-                         -compartment_id=ocid1.compartment.oc1..aaaaaaaaas357j3xdy5grl4dmotmtumrcopqxifuayurkowavjvzrmihfala \
-                         -output_path=tf-export \
-                         -generate_state
+#./terraform-provider-oci -command=export \
+#                         -#compartment_id=ocid1.compartment.oc1..aaaaaaaaas357j3xdy5grl4dmotmtumrcopqxifuayurkowavjvzrmihfala \
+#                         -output_path=tf-export \
+#                         -generate_state
 
-cd tf-export
+#cd tf-export
 
-terraform init
-terraform destroy -auto-approve
+#terraform init
+#terraform destroy -auto-approve
