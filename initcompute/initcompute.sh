b46be376fc5b7e9631d6dd39d60e0f64e4715bd1
tom.eliason <tom.eliason@oracle.com>
2020-11-20 20:27:51 -0700

    update compartment

--- initcompute/initcompute.sh
+++ initcompute/initcompute.sh
@@ -5,16 +5,24 @@ echo 'initcompute'
 # obtain the tenancy OCID from the environment in the OIC Cloud Shell 
 export TF_VAR_tenancy_ocid=$OCI_TENANCY
 
+echo 'working in tenancy ' `oci iam tenancy get --tenancy-id $OCI_TENANCY --query 'data."name"' --output json  `
+
 oci iam compartment list --lifecycle-state ACTIVE --query 'data[*]|[*]."name"' --compartment-id-in-subtree true --output table
-read -p "enter the compartment to create compute: " compartment_name
+read -p "enter the compartment to create compute with oSSH: " compartment_name
 
 echo ${compartment_name}
 export TF_VAR_compartment_name=${compartment_name}
 
-echo $TF_VAR_tenancy_ocid
+#echo $TF_VAR_tenancy_ocid
 
 # change in to the directory to get the GBU compartment OCID
 cd ttt/initcompute/gbuocid
+
+rm -rf .terraform.d 
+rm -rf .terraform
+rm -rf terraform.tfstate
+rm -rf terraform.tfstate.*
+rm -rf .terraform.tfstate.*
 
 # execute terraform; initialize, apply to retrieve the GBU compartment OCID
 terraform init
@@ -25,18 +33,26 @@ export TF_VAR_compartment_id=`terraform output tenancycompartments`
 rm -rf .terraform
 
 # move up a directory
-cd .. 
+cd ../
+pwd 
 
-# cleanup any existing export directory and recreate it
-#rm -rf tf-export 
-#mkdir tf-export 
+# get the most recent Oracle Linux 7 image 
+export instance_image_id=`oci compute image list --compartment-id $TF_VAR_tenancy_ocid --sort-order DESC --sort-by TIMECREATED --query "data[?contains(\"display-name\", 'Oracle-Linux-7')].\"id\"" | awk -F '"' '{print $2}' | awk 'NF' | sed -n '1p'`
+
+rm -rf .terraform.d 
+rm -rf .terraform
+rm -rf terraform.tfstate
+rm -rf terraform.tfstate.*
+rm -rf .terraform.tfstate.*
 
 # initialize terraform to get the provider
 terraform init
 
+terraform plan 
+
 # execute the terraform by applying and auto-approve
 
-terraform apply -auto-approve
+#terraform apply -auto-approve
 
 # find the provider executable and put it in a variable; the export command applies directly to a provider
 #export TPO=`find . -name *terraform-provider-oci*.*`
--- initcompute/vars.tf
+++ initcompute/vars.tf
@@ -1,5 +1,4 @@
 variable region { default = "us-ashburn-1" }
 variable tenancy_ocid { default = "ocid1.tenancy.oc1..aaaaaaaaq3qy3dmnci7vbc6vdzwaptmz6m4u667exhuc2zem4pde4f4fulea" }
-variable compartment_ocid { default = "ocid1.compartment.oc1..aaaaaaaaas357j3xdy5grl4dmotmtumrcopqxifuayurkowavjvzrmihfala" }
-variable instance-public_source_image_id { default = "ocid1.image.oc1.iad.aaaaaaaakcggae4tjdluuizz2if7zivhk2ch47yiqyrrhr7qgygchu7ddyla" }
-variable instance-20200824-2005_source_image_id { default = "ocid1.image.oc1.iad.aaaaaaaakcggae4tjdluuizz2if7zivhk2ch47yiqyrrhr7qgygchu7ddyla" }
+variable compartment_id { default = "ocid1.compartment.oc1..aaaaaaaaas357j3xdy5grl4dmotmtumrcopqxifuayurkowavjvzrmihfala" }
+variable instance_image_id { default = "ocid1.image.oc1.iad.aaaaaaaakcggae4tjdluuizz2if7zivhk2ch47yiqyrrhr7qgygchu7ddyla" }
