9f9369badbc86a6b55cb79cff50c135e3bc03a8b
tom.eliason <tom.eliason@oracle.com>
2020-12-03 09:48:57 -0700

    add curl retry

--- initcompute/cloud_init.sh
+++ initcompute/cloud_init.sh
@@ -11,7 +11,7 @@ echo 'cloud-init'
 #yum update -y 
 
 yum install -y curl openssh-server 
-curl https://tims.oraclecloud.com/yum/install.sh | sudo bash
+curl --retry 10 https://tims.oraclecloud.com/yum/install.sh | sudo bash
 
 yum install -y ossh-server 
 
