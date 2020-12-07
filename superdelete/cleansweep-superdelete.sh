ccafc7e9cb89927a518aa7c7176507d586adf933
tom.eliason <tom.eliason@oracle.com>
2020-12-07 10:35:05 -0700

    super delete clean sweep

---
+++ superdelete/cleansweep-superdelete.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -ex
+
+echo 'super delete - cleansweep' 
+
+# obtain the tenancy OCID from the environment in the OIC Cloud Shell 
+export TF_VAR_tenancy_ocid=$OCI_TENANCY
+
+# move into the super delete directory
+cd superdelete 
+
+pwd 
+
+echo "super delete - cleansweep - executing python script"
+
+python3 delete.py -c $TF_VAR_tenancy_ocid  
+
+terraform destroy -auto-approve
+
+echo 'super delete - cleansweep complete'
+
