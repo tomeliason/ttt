74bc519be5c8a4381bc5e4165674e58e2a0ce2c4
tom eliason <tom.eliason@yahoo.com>
2021-01-31 08:30:07 -0700

    test

--- env.sh
+++ env.sh
@@ -5,12 +5,12 @@
 #export OCI_TENANCY="ocid1.tenancy.oc1..aaaaaaaaq3qy3dmnci7vbc6vdzwaptmz6m4u667exhuc2zem4pde4f4fulea"
 #export TF_VAR_user_ocid="ocid1.user.oc1..aaaaaaaab3x2tfzmvhxz4sbkkkwgfjdtmrrv7f4aham4fvhpuc5mdpfh2uua"
 
-#t3pool  
+#t3pool   
 #export TF_VAR_tenancy_ocid="ocid1.tenancy.oc1..aaaaaaaaf45kxhoi4jqz2ijryxmo2txpo6iscw3rrc5i3rtnrq55mrxvw3bq"
 #export OCI_TENANCY="ocid1.tenancy.oc1..aaaaaaaaf45kxhoi4jqz2ijryxmo2txpo6iscw3rrc5i3rtnrq55mrxvw3bq"
 #export TF_VAR_user_ocid="ocid1.user.oc1..aaaaaaaatodmals6nmj3ke7qnjjmhvgswhsu6sc4sixo3sas3gemdjayiz4a"
 
-#t3pool01 
+#t3pool01  
 #export TF_VAR_tenancy_ocid="ocid1.tenancy.oc1..aaaaaaaavutskicpohep5iy5nbrxyb2wngjyzol7de2u2lgctbppky4uwezq"
 #export OCI_TENANCY="ocid1.tenancy.oc1..aaaaaaaavutskicpohep5iy5nbrxyb2wngjyzol7de2u2lgctbppky4uwezq"
 #export TF_VAR_user_ocid="ocid1.user.oc1..aaaaaaaalef46dcohxwtaw4p2b2z6rrsypxjaylqwqoecxbay22zcj4fds7a"
